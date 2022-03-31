fed1c0bc2338d4a985caeb5214022412b1e719c1
langtonhugh <slangton@nscr.nl>
2022-03-31 14:24:09 +0200

    wide long start

--- scripts/tidytuesday_05apr2022.R
+++ scripts/tidytuesday_05apr2022.R
@@ -55,8 +55,34 @@ fire_wide2_df <- fire_clean_df %>%
 fire_wide2_df %>% 
   pivot_longer(cols = -animal_group_broad, names_to = "property_category", values_to = "yearly_count") %>% 
   ggplot(data = .) +
-  # facet_wrap(~property_category) + # optional
+  facet_wrap(~property_category) + # optional
   geom_col(mapping = aes(x = animal_group_broad, y = yearly_count)) 
+
+# So first, we pivot from wide to long.
+fire_long2_df <- fire_wide2_df %>% 
+  pivot_longer(cols = -animal_group_broad, names_to = "property_category", values_to = "yearly_count") 
+
+# Then we can make either plot fairly easily.
+ggplot(data = fire_long2_df) +
+  facet_wrap(~property_category) + # optional
+  geom_col(mapping = aes(x = animal_group_broad, y = yearly_count))
+
+# Nested data / multilevel example.
+fire_wards_df <- fire_clean_df %>%
+  group_by(borough_code, ward_code, cal_year) %>% 
+  summarise(yearly_count = n()) %>% 
+  # ungroup() %>%
+  # complete(borough_code, cal_year, fill = list(yearly_count = 0)) %>%
+  filter(ward_code != "NULL") %>% 
+  arrange(borough_code, ward_code, cal_year)
+
+fire_wards_wide_df <- fire_wards_df %>% 
+  pivot_wider(names_from = cal_year, values_from = yearly_count, names_prefix = "year_") %>% 
+  # replace_na(replace = list(0)) %>% 
+  arrange(ward_code) 
+  
+  
+
   
 
 
